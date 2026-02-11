d7fa41754e6047e77052c00e05d96031fff94b69
Tesoi25 <soria.glenn1995@gmail.com>
2026-02-11 15:47:55 +0800

    CRUD

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::get('/user', function (Request $request) {
-    return "Hello World, IT322";
+Route::prefix('users')->group(function(){
+    Route::get('/',[UserController::class, 'index']);
+    Route::post('/',[UserController::class, 'store']);
+    Route::get('/{id}',[UserController::class, 'show']);
+    Route::put('/{id}',[UserController::class, 'update']);
+    Route::delete('/{id}',[UserController::class, 'destroy']);
 });
+ 
